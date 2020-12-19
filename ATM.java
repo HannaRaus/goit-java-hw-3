b4c2f10b77e4959d1ce4f6a8ef3f3676f7d10189
Hanna <74618691+HannaRaus@users.noreply.github.com>
2020-12-20 00:23:45 +0200

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,34 @@
+    public int countBanknotes(int sum) {
+
+        int counter = 0;
+        if (sum % 500 == 0) {
+            counter = sum / 500;
+        } else {
+            counter = sum / 500;
+            sum %= 500;
+
+            counter += sum / 200;
+            sum %= 200;
+
+            counter += sum / 100;
+            sum %= 100;
+
+            counter += sum / 50;
+            sum %= 50;
+
+            counter += sum / 20;
+            sum %= 20;
+
+            counter += sum / 10;
+            sum %= 10;
+
+            counter += sum / 5;
+            sum %= 5;
+
+            counter += sum / 2;//23
+            sum %= 2;//0
+
+            counter +=sum;
+        }
+        return counter;
+    }
